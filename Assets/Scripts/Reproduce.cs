0b1230400bcda1991ec3c7c329bc83c219cf647b
milesdanquah <miles.danquah@marquette.edu>
2022-12-02 12:25:02 -0600

    Color Change
    We added color change for each child agent.

--- Assets/Scripts/Reproduce.cs
+++ Assets/Scripts/Reproduce.cs
@@ -11,6 +11,7 @@ public class Reproduce : MonoBehaviour
         myTimePerBirth = timePerBirth+ Random.Range(-2.0f,2.0f);
     }
 
+    public float colorChange = 0.05f;
     public float birthingDistance = 0.5f;
     public float timePerBirth = 3;
     private float myTimePerBirth;
@@ -22,6 +23,9 @@ public class Reproduce : MonoBehaviour
         if(timeSinceBirth>myTimePerBirth){
             GameObject child = Instantiate(myPrefab, new Vector3(transform.position.x+Random.Range(-birthingDistance, birthingDistance), transform.position.y+Random.Range(-birthingDistance, birthingDistance), 0), Quaternion.identity);
             timeSinceBirth=0;
+            Color oldColor = child.GetComponent<SpriteRenderer>().color;
+            child.GetComponent<SpriteRenderer>().color = new Color(oldColor.r+Random.Range(-colorChange, colorChange), oldColor.g+Random.Range(-colorChange, colorChange), oldColor.b+Random.Range(-colorChange, colorChange), oldColor.a+Random.Range(-colorChange, colorChange));
         }
     }
 }
+
